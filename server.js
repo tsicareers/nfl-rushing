3d60877a4c1c056ebefd8365de1439992f7f5a03
Praise <epraise17@yahoo.com>
2020-12-14 21:25:01 -0500

    replace /data endpoint with json contents and create sample table to view in html

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 
 const app = express();
+app.use(express.static("public"));
+
+const fs = require("fs");
+
+var players;
+
+fs.readFile("./rushing.json", (err, data) => {
+    if (err) throw err;
+    players = JSON.parse(data);
+});
 
 app.get("/data", (req, res) => {
-    res.send("hi praise");
+    res.send(players);
 });
 
 app.listen(5001, () => console.log("app is running"));
