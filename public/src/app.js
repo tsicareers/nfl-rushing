fc701617e5260960c22c86dbac2876637d9c0a7e
Praise <epraise17@yahoo.com>
2020-12-14 23:23:13 -0500

    create table in js and load data
    must admit: had some youtube help, this part was a bit confusing

---
+++ public/src/app.js
@@ -0,0 +1,98 @@
+// const { table } = require("console");
+
+{
+    // initial housecleaning..
+    for (const tableDiv of document.querySelectorAll(".nfl-players[data-url")) {
+        // var table = document.getElementById("nfl-players-table");
+        const table = document.createElement("table");
+
+        table.classList.add("nfl-players-table", "sortable");
+        // sorttable.makeSortable(table);
+
+        table.innerHTML = `
+            <thead>
+                <tr>
+                <th class="sorttable_nosort">Player Name</th>
+                <th class="sorttable_nosort">Team</th>
+                <th class="sorttable_nosort" title="Player's Position">Pos</th>
+                <th class="sorttable_nosort" title="Rushing Attempts">Att</th>
+                <th class="sorttable_nosort" title="Rushing Attempts Per Game Average">Att/G</th>
+                <th title="Total Rushing Yards">Yds</th>
+                <th class="sorttable_nosort" title="Rushing Yards Per Game">Yds/G</th>
+                <th class="sorttable_nosort" title="Rushing Average Yards Per Game">Avg</th>
+                <th title="Total Rushing Touchdowns">TD</th>
+                <th title="Longest Rush -- a T represents a touchdown occured">Lng</th>
+                <th class="sorttable_nosort" title="Rushing First Downs">1st</th>
+                <th class="sorttable_nosort" title="Rushing First Down Percentage">1st%</th>
+                <th class="sorttable_nosort" title="Rushing 20+ Yards Each">20+</th>
+                <th class="sorttable_nosort" title="Rushing 40+ Yards Each">40+</th>
+                <th class="sorttable_nosort" title="Rushing Fumbles">FUM</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Loading</td>
+                </tr>
+            </tbody>
+            <tfoot>
+                <tr>
+                    <th>Player Name</th>
+                    <th>Team</th>
+                    <th title="Player's Position">Pos</th>
+                    <th title="Rushing Attempts">Att</th>
+                    <th title="Rushing Attempts Per Game Average">Att/G</th>
+                    <th title="Total Rushing Yards">Yds</th>
+                    <th title="Rushing Yards Per Game">Yds/G</th>
+                    <th title="Rushing Average Yards Per Game">Avg</th>
+                    <th title="Total Rushing Touchdowns">TD</th>
+                    <th title="Longest Rush -- a T represents a touchdown occured">Lng</th>
+                    <th title="Rushing First Downs">1st</th>
+                    <th title="Rushing First Down Percentage">1st%</th>
+                    <th title="Rushing 20+ Yards Each">20+</th>
+                    <th title="Rushing 40+ Yards Each">40+</th>
+                    <th title="Rushing Fumbles">FUM</th>
+                </tr>
+            </tfoot>
+        `;
+
+        // append table to div
+        tableDiv.append(table);
+
+        // update table after setting header/footer on load
+        updateTable(tableDiv);
+    }
+
+    /**
+     * Populate the rows of table
+     * 
+     * @param {HTMLDivElement} tableDiv 
+     */
+    async function updateTable(tableDiv) {
+        // console.log("hi praise");
+
+        const table = tableDiv.querySelector(".nfl-players-table");
+        // const response = await fetch("/data");
+        const response = await fetch(tableDiv.dataset.url);
+        const data = await response.json(); // parse response as json
+
+        // clear table body
+        table.querySelector("tbody").innerHTML = "";
+
+        for (var i = 0; i < data.length; i++) { // looping through the data array
+
+            var object = data[i];
+            // console.log(object);
+
+            var row = table.querySelector("tbody").insertRow();
+
+            for (var property in object) { // iterating the JSON by key(property)
+                // Debug - praise
+                // console.log("property: " + property);
+                // console.log("value: " + object[property]);
+
+                var cell = row.insertCell();
+                cell.innerHTML = object[property];
+            }
+        }
+    }
+}
