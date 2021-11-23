5d89b7f797c25b7980fc93d4b3244195a402e39d
Felipe Kosouski <felipe.kosouski@gmail.com>
2021-11-23 20:53:25 -0300

    Added pagination

--- nfl-rushing-api/app/controllers/api/v1/stats_controller.rb
+++ nfl-rushing-api/app/controllers/api/v1/stats_controller.rb
@@ -1,8 +1,10 @@
 class Api::V1::StatsController < ApplicationController
     def index
         @stats = params[:player].blank? ? RushStat.order(:id) : RushStat.search(params[:player])
-        @stats = @stats.order(params[:sort_by])
-        render json: @stats
+        paginated_stats = @stats.paginate(page: params[:page], per_page: 10).order(params[:sort_by])
+
+        response = {stats: paginated_stats, current_page: paginated_stats.current_page, total_pages: paginated_stats.total_pages}
+        render json: response, status: :ok
     end
 
     def export_csv
@@ -15,7 +17,7 @@ class Api::V1::StatsController < ApplicationController
     private
 
     def stat_params
-        params.require(:rush_stat).permit(:player, :sort_by)
+        params.require(:rush_stat).permit(:player, :sort_by, :page)
     end
     
 end
