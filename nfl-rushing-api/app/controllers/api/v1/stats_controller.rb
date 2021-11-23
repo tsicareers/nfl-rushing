70b5e0fe696a3915ce8de3a8be5252cbe8fd4140
Felipe Kosouski <felipe.kosouski@gmail.com>
2021-11-23 00:02:43 -0300

    Added sorting

--- nfl-rushing-api/app/controllers/api/v1/stats_controller.rb
+++ nfl-rushing-api/app/controllers/api/v1/stats_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::StatsController < ApplicationController
     def index
         @stats = params[:player].blank? ? RushStat.order(:id) : RushStat.search(params[:player])
+        @stats = @stats.order(params[:sort_by])
         render json: @stats
     end
 
     private
 
     def stat_params
-        params.require(:rush_stat).permit(:player)
+        params.require(:rush_stat).permit(:player, :sort_by)
     end
     
 end
