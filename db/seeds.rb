59a0ed3248a2ca8b0b504ae0ac75da046e4dc881
Guilherme Tassinari <guitassinari@gmail.com>
2021-12-09 20:06:34 -0300

    Seeds data from rushing.json in db/seeds.rb (#4)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+rushings_json = File.read('rushing.json')
+rushings_list = JSON.parse(rushings_json)
+
+rushings_list.each do |rushing_data|
+  PlayerRushing.create(
+    player_name: rushing_data["Player"],
+    team_name: rushing_data["Team"],
+    player_position: rushing_data["Pos"],
+    avg_rushing_attempts_per_game: rushing_data["Att"],
+    rushing_attempts: rushing_data["Att/G"],
+    total_rushing_yards: rushing_data["Yds"],
+    avg_yards_per_attempt: rushing_data["Avg"],
+    yards_per_game: rushing_data["Yds/G"],
+    total_touchdowns: rushing_data["TD"],
+    longest_rush: rushing_data["Lng"],
+    first_downs: rushing_data["1st"],
+    first_downs_percentage: rushing_data["1st%"],
+    twenty_yards_rushes: rushing_data["20+"],
+    fourty_yards_rushes: rushing_data["40+"],
+    fumbles: rushing_data["FUM"]
+  )
+end
